32fedec1cb6e5b30203277ba209fd821210b358b
halimov09 <sunnatullohhalimov@gmail.com>
2024-09-26 11:15:23 +0500

    INFRA: Initialize I Unit Test Project

---
+++ UnitTest1.cs/DeleteMe.cs
@@ -0,0 +1,13 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+namespace UnitTest1.cs
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void shouldBeTrue() => Assert.True(true);
+    }
+}
